a97852259dd977322b656c27db8c9992c95cfb6e
Swiftor lilko <swifrorlilko@gmail.com>
2023-01-14 23:29:14 +0200

    ADD: listen for ctrlc, extract disconenct fn, add loop

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
-use std::{error::Error};
-use tokio::{net::{TcpStream}, io::{AsyncWriteExt, AsyncReadExt}};
+use std::{error::Error, sync::Arc};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream, sync::Mutex,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut stream = TcpStream::connect("127.0.0.1:7667").await?;
+    let stream = TcpStream::connect("127.0.0.1:7667").await.expect("[-] Failed to connect to server check if server is running on port 7667");
+    let stream = Arc::new(Mutex::new(stream));
+    let stream_clone= Arc::clone(&stream);
+    tokio::spawn(async move {
+        
+        tokio::signal::ctrl_c().await.unwrap();
+        disconnect(&stream_clone).await.unwrap();
+        std::process::exit(0);
+    });
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
 
-    // Send data to the server
-    let set_msg = "SEwT name John";
-    let get_msg = "GET name";
+        send_message(&stream, &input.trim()).await?;
 
-    send_message(&mut stream, set_msg).await?;
-    let response = read_message(&mut stream).await?;
-    println!("Response: {}", response);
-    send_message(&mut stream, get_msg).await?;
-    let response = read_message(&mut stream).await?;
-    println!("Response: {}", response);
+        let response = read_message(&stream).await?;
+        println!("{}", response);
+    }
+}
 
+pub async fn disconnect(stream: &Arc<Mutex<TcpStream>>) -> Result<(), Box<dyn Error>> {
     let close_msg = "CLOSE";
-    send_message(&mut stream, close_msg).await?;
-
-    // Wait for response
-    let response = read_message(&mut stream).await?;
-    println!("Response: {}", response);
-
-    // Close connection
+    send_message(stream, close_msg).await?;
+    let response = read_message(stream).await?;
+    println!("{}", response);
+    let mut stream = stream.lock().await;
     stream.shutdown().await?;
-
     Ok(())
 }
 
-pub async fn read_message(stream: &mut TcpStream) -> Result<String, Box<dyn Error>> {
+
+pub async fn read_message(stream: &Arc<Mutex<TcpStream>>) -> Result<String, Box<dyn Error>> {
+    let mut stream = stream.lock().await;
     let mut len_bytes = [0; 4];
     let bytes_read = stream.read(&mut len_bytes).await?;
     if bytes_read < 4 {
-        return Err("Failed to read the length of the message".into());
+        return Err("[-] Failed to read the length of the message".into());
     }
     let len = u32::from_be_bytes(len_bytes);
     let mut buffer = vec![0; len as usize];
@@ -42,7 +51,8 @@ pub async fn read_message(stream: &mut TcpStream) -> Result<String, Box<dyn Erro
     Ok(message.to_string())
 }
 
-pub async fn send_message(stream: &mut TcpStream, message: &str) -> Result<(), Box<dyn Error>> {
+pub async fn send_message(stream: &Arc<Mutex<TcpStream>>, message: &str) -> Result<(), Box<dyn Error>> {
+    let mut stream = stream.lock().await;
     let len = message.len() as u32;
     let len_bytes = len.to_be_bytes();
     stream.write_all(&len_bytes).await?;
